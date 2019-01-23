c8591a2e74c4625702d70d421a9ddfbdc32902b5
kendall willard <kendallwillard@hotmail.com>
2019-01-23 23:20:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var name = "Susan"
 var height = "74"
 
 
-//var height test
+/*var height test
 if( name === "Susan") {
   console.log("The name is correct")
 }
 else {
   console.log("Expected the name to be Susan")
-}
+} */
 
 // Don't worry about this
 module.exports = { name, height
